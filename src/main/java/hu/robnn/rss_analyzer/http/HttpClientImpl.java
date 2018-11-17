ecd757c0f07b4bb6429e3e276f236c9c0f172dec
Barsi Gabor Zsolt <gb942tm@gmail.com>
2018-11-17 07:41:25 -0500

    Html reading implemented.

---
+++ src/main/java/hu/robnn/rss_analyzer/http/HttpClientImpl.java
@@ -0,0 +1,34 @@
+package hu.robnn.rss_analyzer.http;
+
+import hu.robnn.rss_analyzer.model.UrlHolder;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+@Component
+public class HttpClientImpl implements HttpClient {
+       @Override
+    public String getWebPageAsString(UrlHolder urlHolder) {
+        try {
+            String line;
+            URL url = new URL(urlHolder.getPageUrl());
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            StringBuilder sb = new StringBuilder();
+
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+            }
+
+            System.out.println("Reading " + urlHolder.getPageUrl() + " succeeded.");
+            br.close();
+
+            return sb.toString();
+        } catch (Exception e) {
+            return "<html><body><p>The given url is invalid.</p></body></html>";
+        }
+    }
+}
